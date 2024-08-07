2193a1332e9787b8e239e3f0fb19039fface8e8a
Zain-Khan602 <zainkhan44607@gmail.com>
2024-08-07 04:05:50 -0700

    I haver learned objects yesterday

---
+++ app.js
@@ -0,0 +1,10 @@
+// let students = ['samad', 'zain', 'moeen','abubakar' ]
+// console.log(students)
+let students = {
+    Name: 'zain',
+    email: 'zainkhan@gamil.com',
+    adress: 'khahhahah karachi',
+    dateofbirth: 170102003
+
+}
+console.log(students.dateofbirth)
